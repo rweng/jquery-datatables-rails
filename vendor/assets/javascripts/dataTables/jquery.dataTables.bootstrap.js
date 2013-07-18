208ccd138e129d26da3717563017581b6548baa9
Adeptus <adeptus@ubuntu.ubuntu-domain>
2013-07-18 12:54:31 +0200

    Add bootstrap-full paginate option(with first and last)

--- vendor/assets/javascripts/dataTables/jquery.dataTables.bootstrap.js
+++ vendor/assets/javascripts/dataTables/jquery.dataTables.bootstrap.js
@@ -91,5 +91,85 @@ $.extend( $.fn.dataTableExt.oPagination, {
 				}
 			}
 		}
-	}
-} );
+	},
+	"bootstrap-full": {
+    "fnInit": function (oSettings, nPaging, fnDraw) {
+      var oLang = oSettings.oLanguage.oPaginate;
+      var fnClickHandler = function (e) {
+        e.preventDefault();
+        if (oSettings.oApi._fnPageChange(oSettings, e.data.action)) {
+          fnDraw(oSettings);
+        }
+      };
+      
+        $(nPaging).addClass('pagination').append(
+                '<ul>' +
+						    '<li class="first disabled"><a href="#">' + oLang.sFirst + '</a></li>' +
+						    '<li class="prev  disabled"><a href="#">' + oLang.sPrevious + '</a></li>' +
+						    '<li class="next  disabled"><a href="#">' + oLang.sNext + '</a></li>' +
+						    '<li class="last  disabled"><a href="#">' + oLang.sLast + '</a></li>' +
+                '</ul>'
+        );
+	      var els = $('a', nPaging);
+	      $(els[0]).bind('click.DT', { action: "first" }, fnClickHandler);
+	      $(els[1]).bind('click.DT', { action: "previous" }, fnClickHandler);
+	      $(els[2]).bind('click.DT', { action: "next" }, fnClickHandler);
+	      $(els[3]).bind('click.DT', { action: "last" }, fnClickHandler);
+      
+    },
+ 
+    "fnUpdate": function (oSettings, fnDraw) {
+      var iListLength = 5;
+      var oPaging = oSettings.oInstance.fnPagingInfo();
+      var an = oSettings.aanFeatures.p;
+      var i, j, sClass, iStart, iEnd, iHalf = Math.floor(iListLength / 2);
+
+      if (oPaging.iTotalPages > 1) {
+        if (oPaging.iTotalPages < iListLength) {
+          iStart = 1;
+          iEnd = oPaging.iTotalPages;
+        }
+	      else if (oPaging.iPage <= iHalf) {
+		      iStart = 1;
+	        iEnd = iListLength;
+	      } else if (oPaging.iPage >= (oPaging.iTotalPages - iHalf)) {
+	        iStart = oPaging.iTotalPages - iListLength + 1;
+	        iEnd = oPaging.iTotalPages;
+	      } else {
+	        iStart = oPaging.iPage - iHalf + 1;
+	        iEnd = iStart + iListLength - 1;
+	      }
+
+  	    for (i = 0, iLen = an.length ; i < iLen ; i++) {
+	        // Remove the middle elements
+		      $('li:gt(1)', an[i]).filter(':not(.next,.last)').remove();
+
+	        // Add the new list items and their event handlers
+					for ( j=iStart ; j<=iEnd ; j++ ) {
+						sClass = (j==oPaging.iPage+1) ? 'class="active"' : '';
+						$('<li '+sClass+'><a href="#">'+j+'</a></li>')
+							.insertBefore( $('.next,.last', an[i])[0] )
+							.bind('click', function (e) {
+								e.preventDefault();
+								oSettings._iDisplayStart = (parseInt($('a', this).text(),10)-1) * oPaging.iLength;
+								fnDraw( oSettings );
+							} );
+					}
+
+        // Add / remove disabled classes from the static elements
+	        if (oPaging.iPage === 0) {
+	          $('.first,.prev', an[i]).addClass('disabled');
+	        } else {
+	          $('.first,.prev', an[i]).removeClass('disabled');
+	        }
+
+	        if (oPaging.iPage === oPaging.iTotalPages - 1 || oPaging.iTotalPages === 0) {
+	          $('.next,.last', an[i]).addClass('disabled');
+	        } else {
+	          $('.next,.last', an[i]).removeClass('disabled');
+	        }
+      	}
+    	}
+    }
+  }
+});
