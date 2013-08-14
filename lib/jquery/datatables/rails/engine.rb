d3dab9534b32342069625c6a3dd525e7c025d5a1
dow <dowdrake@msn.com>
2013-08-14 09:21:16 -0700

    try fixing issue of images not included in manifest or copied to public/assets during precompilation with Rails 4.0

--- lib/jquery/datatables/rails/engine.rb
+++ lib/jquery/datatables/rails/engine.rb
@@ -4,6 +4,23 @@ module Jquery
     if defined?(::Rails) and Gem::Requirement.new('>= 3.1').satisfied_by?(Gem::Version.new ::Rails.version)
       module Rails
         class Engine < ::Rails::Engine
+          # add initializer so that images get added to the manifest and placed in public/assets in Rails 4
+          initializer "jhtmlarea.assets.precompile" do |app|
+            app.config.assets.precompile += 
+              %w( back_disabled.png
+                  back_enabled_hover.png
+                  back_enabled.png
+                  extras
+                  favicon.ico
+                  forward_disabled.png
+                  forward_enabled_hover.png
+                  forward_enabled.png
+                  sort_asc_disabled.png
+                  sort_asc.png
+                  sort_both.png
+                  sort_desc_disabled.png
+                  sort_desc.png )
+          end
         end
       end
     end
