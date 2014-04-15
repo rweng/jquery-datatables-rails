f5e9ef38f4ab41e495ac822b64cd6a8d08548ec3
Ian Smith <ian@allur.com>
2014-04-15 16:52:20 -0700

    Added support for fnGetHiddenNodes

---
+++ app/assets/javascripts/dataTables/jquery.dataTables.api.fnGetHiddenNodes.js
@@ -0,0 +1,19 @@
+jQuery.fn.dataTableExt.oApi.fnGetHiddenNodes = function ( oSettings )
+{
+    /* Note the use of a DataTables 'private' function thought the 'oApi' object */
+    var anNodes = this.oApi._fnGetTrNodes( oSettings );
+    var anDisplay = $('tbody tr', oSettings.nTable);
+
+    /* Remove nodes which are being displayed */
+    for ( var i=0 ; i<anDisplay.length ; i++ )
+    {
+        var iIndex = jQuery.inArray( anDisplay[i], anNodes );
+        if ( iIndex != -1 )
+        {
+            anNodes.splice( iIndex, 1 );
+        }
+    }
+
+    /* Fire back the array to the caller */
+    return anNodes;
+};
