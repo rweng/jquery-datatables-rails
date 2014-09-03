b74840fd12f8b32a8b070939f47ac44d2968e410
James Coleman <jtc331@gmail.com>
2014-09-03 16:50:25 -0400

    Don't attach filter delay unless filtering is present.
    This avoids accidentally binding to random inputs on the page and breaking things. Yes, it's true that you should just not activate the filter delay unless you've got filtering enabled, but in the interest of the best possible default experience, that shouldn't cause side effects.

--- app/assets/javascripts/dataTables/jquery.dataTables.api.fnSetFilteringDelay.js
+++ app/assets/javascripts/dataTables/jquery.dataTables.api.fnSetFilteringDelay.js
@@ -7,24 +7,27 @@ jQuery.fn.dataTableExt.oApi.fnSetFilteringDelay = function ( oSettings, iDelay )
 
     this.each( function ( i ) {
         $.fn.dataTableExt.iApiIndex = i;
-        var
-            $this = this,
-            oTimerId = null,
-            sPreviousSearch = null,
-            anControl = $( 'input', _that.fnSettings().aanFeatures.f );
+        var aanFeatures = _that.fnSettings().aanFeatures;
+        if (aanFeatures.f) {
+            var
+                $this = this,
+                oTimerId = null,
+                sPreviousSearch = null,
+                anControl = $( 'input',  aanFeatures.f );
 
             anControl.off( 'keyup search input' ).on( 'keyup search input', function() {
-            var $$this = $this;
+                var $$this = $this;
 
-            if (sPreviousSearch === null || sPreviousSearch != anControl.val()) {
-                window.clearTimeout(oTimerId);
-                sPreviousSearch = anControl.val();
-                oTimerId = window.setTimeout(function() {
-                    $.fn.dataTableExt.iApiIndex = i;
-                    _that.fnFilter( anControl.val() );
-                }, iDelay);
-            }
-        });
+                if (sPreviousSearch === null || sPreviousSearch != anControl.val()) {
+                    window.clearTimeout(oTimerId);
+                    sPreviousSearch = anControl.val();
+                    oTimerId = window.setTimeout(function() {
+                        $.fn.dataTableExt.iApiIndex = i;
+                        _that.fnFilter( anControl.val() );
+                    }, iDelay);
+                }
+            });
+        }
 
         return this;
     } );
